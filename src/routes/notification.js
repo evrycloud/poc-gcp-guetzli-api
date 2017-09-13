0fcf2c9d4d27a99223eb626a0c19176d323ba361
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2017-09-13 12:39:42 +0200

    enhance sse

--- src/routes/notification.js
+++ src/routes/notification.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const PubSub = require('@google-cloud/pubsub');
 const sseChannel = require('sse-channel');
 const config = require('config');
@@ -7,13 +8,21 @@ const pubsub = PubSub({ projectId: config.get('project.id') });
 const subscribe = pubsub.subscription(config.get('pubsub.subscription'));
 
 const channel = new sseChannel({
-    cors: { origins: '*' }
+    historySize: 20,
+    cors: {
+        origins: '*'
+    }
 });
 
 subscribe.on('message', msg => {
     msg.ack();
 
-    channel.send(msg.data.toString('utf8'));
+    const data = msg.data.toString('utf8');
+
+    channel.send({
+        data: data,
+        id: crypto.createHash('md5').update(data).digest('hex')
+    });
 });
 
 module.exports = (req, res) => {
