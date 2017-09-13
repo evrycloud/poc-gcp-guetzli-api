cba7524116fc7665acf483576e5811d6d5a8e3a2
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2017-09-13 19:35:18 +0200

    remove test logic

--- src/start-server.js
+++ src/start-server.js
@@ -5,14 +5,11 @@ const { router, get, post } = require('microrouter');
 const notification = require('./routes/notification');
 const { list, upload } = require('./routes/images');
 
-const hello = (req, res) => send(res, 200, `Hello ${req.params.who}`);
-
 const notfound = (req, res) => send(res, 404, 'Not found route');
 
 module.exports = function startServer(port) {
     return micro(
         router(
-            get('/hello/:who', hello),
             get('/notification', notification),
             get('/images/list', list),
             post('/images/upload', upload),
